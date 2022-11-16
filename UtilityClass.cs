8489a39f3d2eb7a23df2f0aa9f02e93e4cf602e9
rmmong2 <rmmong2@gmail.com>
2022-11-16 14:21:57 -0500

    Added text to apparel tab window. Shows tattered apparel. Need to solve issue that only shows 1 tattered item

--- UtilityClass.cs
+++ UtilityClass.cs
@@ -11,7 +11,22 @@ namespace TatteredUI
 {
     class UtilityClass
     {
-        public static void Setup()
+		public static Color BgColorButton
+		{
+			get
+			{
+				return new Color(0.1640625f, 0.1640625f, 0.1640625f);
+			}
+		}
+
+		public static Color BgColorDarkButton
+		{
+			get
+			{
+				return new Color(0.5f, 0.5f, 0.5f);
+			}
+		}
+		public static void Setup()
         {
 
         }
